8a15daf43c38f7931b748e62c729e75a0843cca9
Alex.Watts <Alex.Watts@kiewit.com>
2021-07-12 11:43:36 -0600

    removed scale var

--- role.harvester.js
+++ role.harvester.js
@@ -25,7 +25,7 @@ var roleHarvester = {
     spawn: function() {
         var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
         //console.log('Harvesters: ' + harvesters.length);
-        if(harvesters.length < (scale*2)) {
+        if(harvesters.length < (2)) {
             var newName = 'Harvester' + Game.time;
            //console.log('Spawning new harvester: ' + newName);
             Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,CARRY,MOVE,MOVE], newName, 
