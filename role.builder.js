e7969a060d4f08eaf80eb0a0b096bb2bbfc0f55c
Alex.Watts <Alex.Watts@kiewit.com>
2021-07-14 16:18:57 -0600

    dump extra resource into extensions and spawn, else recycle to fixer

--- role.builder.js
+++ role.builder.js
@@ -20,8 +20,21 @@ var roleBuilder = {
                 }
             }
             else {
-                creep.say('now fixer');
-                creep.memory.role = 'fixer';
+                var targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                    }
+                });
+                if(targets.length > 0) {
+                    if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                    }
+                }
+                else {
+                    creep.say('now fixer');
+                    creep.memory.role = 'fixer';
+                }
             }
         }
         else {
