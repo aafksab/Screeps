af5b30bc0d1bbc701c8fc819e74034c0c1e29f01
aafksab <aafksab@gmail.com>
2021-07-15 09:55:41 -0600

    added xfer role to move energy from container to spawn or storage

---
+++ role.xfer.js
@@ -0,0 +1,37 @@
+var xfer = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+	    if(creep.store.getFreeCapacity() > 0) {
+            var sources = creep.room.find(FIND_CONTAINERS);
+            if(creep.transfer(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+        }
+        else {
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN ) &&
+                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                }
+            });
+            if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
+        }
+	},
+    spawn: function(scale) {
+        var xfer = _.filter(Game.creeps, (creep) => creep.memory.role == 'xfer');
+        console.log('xfer:' + xfer.length + ' Max:' + scale);
+        if(harvesters.length < scale) {
+            var newName = 'xfer' + Game.time;
+           //console.log('Spawning new harvester: ' + newName);
+            Game.spawns['Spawn1'].spawnCreep([WORK,WORK,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE], newName, 
+                {memory: {role: 'xfer'}});
+        }
+    }
+};
+
+module.exports = xfer;
