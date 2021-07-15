d658cd75cf6fa9315e1a3362278b07a643e49b38
aafksab <aafksab@gmail.com>
2021-07-15 09:40:52 -0600

    updated role to dump energy into container if extensions and spawn are full

--- role.harvester.js
+++ role.harvester.js
@@ -20,6 +20,19 @@ var roleHarvester = {
                     creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             }
+            else {
+                var targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_CONTAINER ) &&
+                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                    }
+                });
+                if(targets.length > 0) {
+                    if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                    }
+                }
+            }
         }
 	},
     spawn: function(scale) {
